3afa9b6e6f94cf6204b851dd618f94d2fb96e50a
wichai <wichai.tos@gmail.com>
2022-03-30 09:47:04 +0700

    Add include file for accAlert

---
+++ src/accAlert.h
@@ -0,0 +1,14 @@
+// Define operations
+#define  GET_VALUE     1
+#define  SET_VALUE     0
+#define  GET_NO_OBJECT 0
+#define  COUNT_OBJECT  1
+#define  CHK_ACCIDENT  2
+#define  MAX_OBJECT 20
+
+// Define variable for Crash Algorithm
+#define  OBJECT_SIZE 500
+#define  MID_SCREEN_X 800 //Resolution /2
+#define  MID_SCREEN_LIMIT 50  
+#define  OBJECT_OVERLAPX 100
+#define  OBJECT_OVERLAPY 100
--- src/ivas_airender.cpp
+++ src/ivas_airender.cpp
@@ -26,6 +26,7 @@
 #include <chrono>
 
 #include "ivas_airender.hpp"
+#include "accAlert.h"
 
 int log_level = LOG_LEVEL_WARNING;
 
@@ -38,24 +39,7 @@ using namespace std;
 #define MAX_ALLOWED_CLASS 20
 #define MAX_ALLOWED_LABELS 20
 
-// invtos --------------
-
-#define  GET_VALUE     1
-#define  SET_VALUE     0
-#define  GET_NO_OBJECT 0
-#define  COUNT_OBJECT  1
-#define  CHK_ACCIDENT  2
-#define  MAX_OBJECT 20
-
-// Define variable for Crash Algorithm
-#define  OBJECT_SIZE 500
-#define  MID_SCREEN_X 800 //Resolution /2
-#define  MID_SCREEN_LIMIT 50  
-#define  OBJECT_OVERLAPX 100
-#define  OBJECT_OVERLAPY 100
-
-
-// ---------------------
+ 
 
 struct color
 {
